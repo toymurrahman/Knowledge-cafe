c235ee4a4171287e6d37d103b66dc654bc49a84b
toymurrahman <toymurrahman2@gmail.com>
2025-01-21 22:34:42 +0600

    blogs data created

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+             <h1 className='text-6xl bg-transparent'>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
